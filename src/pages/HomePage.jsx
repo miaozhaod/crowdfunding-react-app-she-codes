53a24d95c9bfad1044c9942589c209622230d5df
Miao Zhao <miao_zhao@yahoo.com>
2022-08-07 08:25:53 +1000

    Update `HomePage` to fetch projectData from remote instead of using dummy data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { allProjects } from "../data";
+import React, { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectaCard";
 
 export default function HomePage() {
+  const [projectData, setProjectData] = useState([]);
+
+  useEffect(() => {
+    // get project data from api when we mount our HomePage
+    fetch(`${process.env.REACT_APP_API_URL}/projects/`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log("data is:", data);
+        setProjectData(data);
+      })
+      .catch(err => {
+        console.log("err is: ", err);
+      });
+  }, []);
+
   return (
     <div>
-      {allProjects.map((project, index) => (
+      {projectData.map((project, index) => (
         <ProjectCard key={index} project={project} />
       ))}
     </div>
