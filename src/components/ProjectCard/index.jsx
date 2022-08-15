6104c1a9d8e15a6deb70a581c0dde528b6e5a037
Miao Zhao <miao_zhao@yahoo.com>
2022-08-15 19:24:40 +1000

    Update `ProjectCard` content and style alinging to Figma design

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,13 +1,60 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./ProjectCard.css";
 
 export default function ProjectCard({ project }) {
-  console.log(project);
+  const { id, image, location, owner, title } = project;
+  const [ownerName, setOwnerName] = useState("");
+  const [pledgeStatus, setPledgeStatus] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/users/${owner}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setOwnerName(data.username);
+      })
+      .catch(err => console.log("user err", err));
+
+    fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        const { goal, pledges } = data;
+        let pledgeNumberArr = [];
+        if (pledges.length > 0) {
+          pledges.map(pledge => pledgeNumberArr.push(pledge.amount));
+        } else {
+          pledgeNumberArr.push(0);
+        }
+        const pledgeSum = pledgeNumberArr.reduce((accumulator, value) => {
+          return accumulator + value;
+        });
+        setPledgeStatus(((pledgeSum / goal) * 100).toFixed(0));
+      })
+      .catch(err => console.log("project err", err));
+  }, [project]);
+
   return (
-    <div className="project-card">
-      <Link to={`/project/${project.id}`}>
-        <img src={project.image} alt="" />
-        <h3>{project.title}</h3>
+    <div className="project-card-container">
+      <Link to={`/project/${id}`}>
+        <img src={image} alt={title} />
+        <div className="project-title-n-pledge">
+          <h3>{title}</h3>
+          <div className="pledge-bar-base">
+            <p className="pledge-bar-percentage-number">{pledgeStatus}%</p>
+            <div
+              className="pledge-bar-percentage-bar"
+              style={{ width: `${pledgeStatus}%` }} // better way?
+            ></div>
+          </div>
+        </div>
+        <div className="project-owner-n-location">
+          <p>{ownerName}</p>
+          <p>{location}</p>
+        </div>
       </Link>
     </div>
   );
