ffa658e9fb664f2c9741e54d16cb43bb785a538b
Miao Zhao <miao_zhao@yahoo.com>
2022-08-16 18:37:27 +1000

    Update `LoginForm` with login function

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
-  const [credentials, setCredentials] = useState({});
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
   const handleChange = event => {
     const { id, value } = event.target;
@@ -9,11 +14,50 @@ export default function LoginForm() {
     setCredentials({ ...credentials, [id]: value });
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
+  const postData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/api-token-auth/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      }
+    );
+
+    return response.json();
   };
+
+  // fetch(`${process.env.REACT_APP_API_URL}/api-token-auth/`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(credentials),
+  // })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     window.localStorage.setItem("token", data.token);
+  //     console.log("Found data: ", data);
+  //   });
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    console.log("Loggin in with: ", credentials);
+    if (credentials.username && credentials.password) {
+      const data = await postData();
+      window.localStorage.setItem("token", data.token);
+      navigate("/");
+      // postData().then(data => {
+      //   window.localStorage.setItem("token", data.token);
+      //   console.log("Found data: ", data);
+      // });
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" onChange={handleChange} />
@@ -23,9 +67,7 @@ export default function LoginForm() {
         <input type="password" id="password" onChange={handleChange} />
       </div>
       <div>
-        <button type="submit" onSubmit={handleSubmit}>
-          Login
-        </button>
+        <button type="submit">Login</button>
       </div>
     </form>
   );
