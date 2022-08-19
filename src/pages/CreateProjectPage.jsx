713baa509b99b8d11dbf79728555f8ec74b854fb
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 06:09:07 +1000

    Create CreateProjectPage.jsx

---
+++ src/pages/CreateProjectPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "../components/common/Container";
+import Banner from "../components/common/Banner";
+import CreateProjectForm from "../components/CreateProjectForm";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Container bg={true} variant="banner">
+        <Banner heading="Plan your exhibition">
+          Let’s start to plan for your exihibition now!
+        </Banner>
+      </Container>
+      <Container>
+        <CreateProjectForm />
+      </Container>
+    </>
+  );
+}
