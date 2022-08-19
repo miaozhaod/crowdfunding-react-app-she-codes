a6b5f9bccb6285e63f40666764af49629a457ae1
Miao Zhao <miao_zhao@yahoo.com>
2022-08-19 21:48:03 +1000

    Update `RoundButton` with onClick

--- src/components/common/RoundButton/index.jsx
+++ src/components/common/RoundButton/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./RoundButton.css";
 
-export default function RoundButton({ children, variant }) {
+export default function RoundButton({ children, variant, onClick }) {
   return (
     <button
       className={`round-button ${
         variant === "primary" ? "primary" : "secondary"
       }`}
+      onClick={onClick}
     >
       {children}
     </button>
