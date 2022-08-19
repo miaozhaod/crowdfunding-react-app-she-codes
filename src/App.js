d04966f80ef6d3220948ebfd6fc3789a12bc8089
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 06:08:58 +1000

    Add `create-project` route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/common/Nav";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
+import CreateProjectPage from "./pages/CreateProjectPage";
 import LoginPage from "./pages/LoginPage";
 import EditProfilePage from "./pages/EditProfilePage";
 import Footer from "./components/common/Footer";
@@ -16,6 +17,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/project/:id" element={<ProjectPage />} />
+          <Route path="/create-project" element={<CreateProjectPage />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route path="/edit-profile" element={<EditProfilePage />} />
         </Routes>
