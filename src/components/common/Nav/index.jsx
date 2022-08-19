7ee7a61242fbda26a6c46eab60181fa336e55b1c
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 06:10:32 +1000

    Update `nav` with logout redirect and add link to `create-project`

--- src/components/common/Nav/index.jsx
+++ src/components/common/Nav/index.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../../assets/img/logo.svg";
 import Container from "../Container";
 import RoundButton from "../RoundButton";
 import "./Nav.css";
 
 export default function Nav() {
+  const navigate = useNavigate();
   const [navAvatar, setNavAvatar] = useState("");
   const [navUsername, setNavUsername] = useState("");
-  const [navDropdown, setNavDropdown] = useState(false);
   const loginStatus = window.localStorage.getItem("login");
   const userId = window.localStorage.getItem("user_id");
 
@@ -49,13 +49,14 @@ export default function Nav() {
           ) : (
             <div className="menu_item_auth_group">
               <Link to="/edit-profile"> Edit Profile</Link>
-              <Link to="/login">
+              <Link to="/create-project">
                 <RoundButton variant="primary">Create Project</RoundButton>
               </Link>
               <RoundButton
                 variant="secondary"
                 onClick={() => {
                   localStorage.clear();
+                  navigate("/");
                   window.location.reload();
                 }}
               >
