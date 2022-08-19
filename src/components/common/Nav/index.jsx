d964360b9b4fdd50bb81739f407fac36a1dbe6ff
Miao Zhao <miao_zhao@yahoo.com>
2022-08-19 21:47:36 +1000

    Update `Nav` to reflect login true

--- src/components/common/Nav/index.jsx
+++ src/components/common/Nav/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../../assets/img/logo.svg";
 import Container from "../Container";
@@ -6,6 +6,27 @@ import RoundButton from "../RoundButton";
 import "./Nav.css";
 
 export default function Nav() {
+  const [navAvatar, setNavAvatar] = useState("");
+  const [navUsername, setNavUsername] = useState("");
+  const [navDropdown, setNavDropdown] = useState(false);
+  const loginStatus = window.localStorage.getItem("login");
+  const userId = window.localStorage.getItem("user_id");
+
+  if (userId) {
+    const getUser = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/users/${userId}`
+      );
+      return response.json();
+    };
+
+    getUser().then(data => {
+      const { avatar, username } = data;
+      setNavAvatar(avatar);
+      setNavUsername(username);
+    });
+  }
+
   return (
     <nav>
       <Container variant="nav">
@@ -16,14 +37,32 @@ export default function Nav() {
           <Link to="/" className="menu_item_home">
             Explore Exhibitions
           </Link>
-          <div className="menu_item_auth_group">
-            <Link to="/login">
-              <RoundButton variant="primary">Login</RoundButton>
-            </Link>
-            <Link to="/login">
-              <RoundButton variant="secondary">Sign up</RoundButton>
-            </Link>
-          </div>
+          {!loginStatus ? (
+            <div className="menu_item_auth_group">
+              <Link to="/login">
+                <RoundButton variant="primary">Login</RoundButton>
+              </Link>
+              <Link to="/login">
+                <RoundButton variant="secondary">Sign up</RoundButton>
+              </Link>
+            </div>
+          ) : (
+            <div className="menu_item_auth_group">
+              <Link to="/edit-profile"> Edit Profile</Link>
+              <Link to="/login">
+                <RoundButton variant="primary">Create Project</RoundButton>
+              </Link>
+              <RoundButton
+                variant="secondary"
+                onClick={() => {
+                  localStorage.clear();
+                  window.location.reload();
+                }}
+              >
+                Log out
+              </RoundButton>
+            </div>
+          )}
         </div>
       </Container>
     </nav>
