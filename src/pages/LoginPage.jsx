4aed277e11fa8e94bc9d23910650e45fca719cc0
Miao Zhao <miao_zhao@yahoo.com>
2022-08-19 19:58:26 +1000

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import Container from "../components/common/Container";
+import Banner from "../components/common/Banner";
 import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <Container bg={true} variant="banner">
+        <Banner heading="Login Now!">Welcome back 👐</Banner>
+      </Container>
+      <Container>
+        <LoginForm />
+      </Container>
+    </>
+  );
 }
