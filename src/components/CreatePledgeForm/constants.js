9620e768f23e3364b62953e7b54b8a62996bafa9
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 23:05:59 +1000

    Create `CreatePledgeForm`

---
+++ src/components/CreatePledgeForm/constants.js
@@ -0,0 +1,25 @@
+export const createPledgeFormInputFields = [
+  {
+    type: "number",
+    id: "amount",
+    label: "Amount",
+    placeholder: "How much you would like to pledge ...",
+  },
+  {
+    type: "textarea",
+    id: "comment",
+    label: "Comment",
+    placeholder: "Leave a comment to your pledge ...",
+  },
+];
+
+export const initialLocationStates = [
+  { Adelaide: false },
+  { Brisbane: false },
+  { Canberra: false },
+  { Darwin: false },
+  { Hobart: false },
+  { Melbourne: false },
+  { Perth: false },
+  { Sydney: false },
+];
---
+++ src/components/CreatePledgeForm/index.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../common/Form/Input";
+import SubmitButton from "../common/Form/SubmitButton";
+import { createPledgeFormInputFields } from "./constants";
+
+export default function CreatePledgeForm({ project_id }) {
+  const navigate = useNavigate();
+  const token = window.localStorage.getItem("token");
+  const [pledgeDetails, setPledgeDetails] = useState({
+    amount: "",
+    comment: "",
+    anonymous: false,
+    project_id,
+  });
+
+  const [submitMessage, setSubmitMessage] = useState("");
+  const [submitResult, setSubmitResult] = useState("");
+
+  const handleChange = event => {
+    const { id, value } = event.target;
+
+    let passValue;
+    id === "amount" ? (passValue = parseInt(value)) : (passValue = value);
+    setPledgeDetails({ ...pledgeDetails, [id]: passValue });
+  };
+  const postData = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/pledges/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pledgeDetails),
+    });
+    return response.json();
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    console.log("Create pledge with pledgeDetails: ", pledgeDetails);
+    if (pledgeDetails.amount && pledgeDetails.comment) {
+      const data = await postData();
+      console.log("Create pledge response data: ... ", data);
+      setSubmitMessage(
+        "Yah! Pledge created successfully, we're directing you to the poject page ..."
+      );
+      setSubmitResult("success");
+      navigate(`/project/${project_id}`);
+    } else {
+      setSubmitMessage("Please enter all fields");
+      setSubmitResult("fail");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="create-project-form">
+      {createPledgeFormInputFields.map((field, index) => {
+        const { type, id, label, placeholder } = field;
+        return (
+          <Input
+            key={index}
+            type={type}
+            id={id}
+            label={label}
+            placeholder={placeholder}
+            onChange={handleChange}
+          />
+        );
+      })}
+
+      <SubmitButton
+        variant="primary"
+        submitMessage={submitMessage}
+        result={submitResult}
+      >
+        Pledge Now !
+      </SubmitButton>
+    </form>
+  );
+}
