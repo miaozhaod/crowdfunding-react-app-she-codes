6f6c15bc34c88ce60048d1b0d4801e40155fd7a2
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 20:23:03 +1000

    Update ProjectPage.jsx

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Container from "../components/common/Container";
+import Banner from "../components/common/Banner";
+import ProjectPageContent from "../components/ProjectPageContent";
 
 export default function ProjectPage() {
-  console.log("this is project page");
-  const [projectData, setProjectData] = useState([]);
+  const [projectData, setProjectData] = useState({});
   const { id } = useParams();
-  console.log("api url: ", `${process.env.REACT_APP_API_URL}/projects/${id}`);
+  const [ownerId, setOwnerId] = useState("");
+  const [ownerName, setOwnerName] = useState("");
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`)
@@ -13,32 +16,36 @@ export default function ProjectPage() {
         return res.json();
       })
       .then(data => {
-        console.log("data", data);
+        setOwnerId(data.owner);
         setProjectData(data);
       })
       .catch(err => console.log("err", err));
-  }, []);
+  }, [id]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/users/${ownerId}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setOwnerName(data.username);
+      })
+      .catch(err => console.log("user err", err));
+  }, [ownerId]);
 
   console.log("projectData", projectData);
-
+  const { title, location } = projectData;
   return (
     <>
-      <h1>{projectData.title}</h1>
-      <h3>Created at: {projectData.date_created}</h3>
-      <h3>{`Status: ${projectData.is_open}`}</h3>
-      <img src={`${projectData.image}`} alt="" />
-      <h3>Pledges:</h3>
-      <ul>
-        {projectData.pledges
-          ? projectData.pledges.map((pledgeData, index) => {
-              return (
-                <li key={index}>
-                  {pledgeData.amount} from {pledgeData.supporter}
-                </li>
-              );
-            })
-          : "No Pledges"}
-      </ul>
+      <Container variant="banner" bg={true}>
+        <Banner variant="banner-tags" heading={title}>
+          <p>{ownerName ? ownerName : "fetching..."}</p>
+          <p>{location ? location : "fetching..."}</p>
+        </Banner>
+      </Container>
+      <Container>
+        <ProjectPageContent projectData={projectData} />
+      </Container>
     </>
   );
 }
