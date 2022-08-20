262fc43755bd5e44593d98270f0bf9fcc2d63093
Miao Zhao <miao_zhao@yahoo.com>
2022-08-20 22:26:06 +1000

    Add `CreatePledge` route to App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/common/Nav";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
 import CreateProjectPage from "./pages/CreateProjectPage";
+import CreatePledgePage from "./pages/CreatePledgePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import EditProfilePage from "./pages/EditProfilePage";
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/project/:id" element={<ProjectPage />} />
           <Route path="/create-project" element={<CreateProjectPage />} />
+          <Route path="/create-pledge/:id" element={<CreatePledgePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/edit-profile" element={<EditProfilePage />} />
