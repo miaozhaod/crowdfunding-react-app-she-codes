e6c9a8ccab5f625d1ed1b305f46d4081f6849c8f
Miao Zhao <miao_zhao@yahoo.com>
2022-08-21 11:16:26 +1000

    Update EditProfilePage.jsx

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Container from "../components/common/Container";
 import Banner from "../components/common/Banner";
-import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
@@ -9,7 +8,7 @@ export default function LoginPage() {
       <Container bg={true} variant="banner">
         <Banner heading="Profile"></Banner>
       </Container>
-      <Container>Edit Profile</Container>
+      <Container>Edit Profile WIP</Container>
     </>
   );
 }
