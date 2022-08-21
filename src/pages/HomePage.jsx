64a6e27e342d052b8c20bd12051841b946b4a59d
Miao Zhao <miao_zhao@yahoo.com>
2022-08-21 10:10:28 +1000

    Refactor `Homepage` projects with `getAllProjects` service

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { getAllProjects } from "../services/getAllProjects";
 import Banner from "../components/common/Banner";
 import Container from "../components/common/Container";
 import GridThreeCol from "../components/common/GridThreeCol";
@@ -8,17 +9,9 @@ export default function HomePage() {
   const [projectData, setProjectData] = useState([]);
 
   useEffect(() => {
-    // get project data from api when we mount our HomePage
-    fetch(`${process.env.REACT_APP_API_URL}/projects/`)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setProjectData(data);
-      })
-      .catch(err => {
-        console.log("err is: ", err);
-      });
+    getAllProjects().then(data => {
+      setProjectData(data);
+    });
   }, []);
 
   return (
