17c5827370d712ad6b932cc1ae64bf187a7e19b0
Miao Zhao <miao_zhao@yahoo.com>
2022-08-21 11:05:13 +1000

    Add `checkbox` type into `Input` component

--- src/components/common/Form/Input/index.jsx
+++ src/components/common/Form/Input/index.jsx
@@ -13,6 +13,11 @@ export default function Input({ type, id, label, placeholder, onChange }) {
             onChange={onChange}
             placeholder={placeholder}
           />
+        ) : type === "checkbox" ? (
+          <div className="form-field-checkbox">
+            <input type={type} id={id} onChange={onChange} />
+            <label htmlFor={id}>{placeholder}</label>
+          </div>
         ) : (
           <input
             type={type}
