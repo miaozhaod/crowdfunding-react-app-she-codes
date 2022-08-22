20d73e44c38b7f605d60c2fc29294d225864c171
Miao Zhao <miao_zhao@yahoo.com>
2022-08-22 22:57:55 +1000

    Add confirmation modal to delete project action

--- src/components/ProjectCard/UserProjectCard.jsx
+++ src/components/ProjectCard/UserProjectCard.jsx
@@ -8,6 +8,7 @@ import { deleteProjectById } from "../../services/deleteProjectById";
 export default function ProjectCard({ project }) {
   const { id, image, location, title } = project;
   const token = window.localStorage.getItem("token");
+  const [modal, setModal] = useState(false);
   const [pledgeStatus, setPledgeStatus] = useState("");
 
   useEffect(() => {
@@ -33,29 +34,49 @@ export default function ProjectCard({ project }) {
   };
 
   return (
-    <div className="project-card-container">
-      <img src={image} alt={title} />
-      <div className="project-title-n-pledge">
+    <>
+      <div className="project-card-container">
+        <img src={image} alt={title} />
+        <div className="project-title-n-pledge">
+          <div>
+            <h4>{title}</h4>
+            <p>{location ? location : "fetching..."}</p>
+          </div>
+          <div className="pledge-bar-base">
+            <p className="pledge-bar-percentage-number">{pledgeStatus}%</p>
+            <div
+              className="pledge-bar-percentage-bar"
+              style={{ width: `${pledgeStatus}%` }}
+            ></div>
+          </div>
+        </div>
         <div>
-          <h4>{title}</h4>
-          <p>{location ? location : "fetching..."}</p>
-        </div>
-        <div className="pledge-bar-base">
-          <p className="pledge-bar-percentage-number">{pledgeStatus}%</p>
-          <div
-            className="pledge-bar-percentage-bar"
-            style={{ width: `${pledgeStatus}%` }}
-          ></div>
+          <SubmitButton variant="danger" onClick={() => setModal(true)}>
+            Delete Project
+          </SubmitButton>
+          <Link to={`/update-project/${id}`}>
+            <SubmitButton variant="primary">Update Project</SubmitButton>
+          </Link>
         </div>
       </div>
-      <div>
-        <SubmitButton variant="danger" onClick={handleDeleteClick}>
-          Delete Project
-        </SubmitButton>
-        <Link to={`/update-project/${id}`}>
-          <SubmitButton variant="primary">Update Project</SubmitButton>
-        </Link>
+      <div
+        className={`delete-confirm-modal ${
+          modal ? "modal-display" : "modal-hidden"
+        }`}
+      >
+        <p>
+          Do you confirm to delete this project? Once it is deleted, you cannot
+          recover it
+        </p>
+        <div className="delete-confirm-modal-button-group">
+          <SubmitButton onClick={() => setModal(false)}>
+            No, not now
+          </SubmitButton>
+          <SubmitButton variant="primary" onClick={handleDeleteClick}>
+            Yes, delete now
+          </SubmitButton>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
