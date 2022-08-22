5eb23e4f8d1ef84c75856ddc8e220bf89d049fc5
Miao Zhao <miao_zhao@yahoo.com>
2022-08-22 22:12:47 +1000

    Update `Input` component with new props and variants

--- src/components/common/Form/Input/index.jsx
+++ src/components/common/Form/Input/index.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import "./Input.css";
 
-export default function Input({ type, id, label, placeholder, onChange }) {
+export default function Input({
+  variant,
+  type,
+  id,
+  label,
+  placeholder,
+  defaultValue,
+  defaultChecked,
+  onChange,
+  onBlur,
+}) {
   return (
     <>
       <div className="form-field">
-        <label htmlFor={id}>{label}</label>
+        <label htmlFor={id} className="form-input-label">
+          {label}
+        </label>
         {type === "textarea" ? (
           <textarea
             type={type}
             id={id}
             onChange={onChange}
+            onBlur={onBlur}
             placeholder={placeholder}
+            defaultValue={defaultValue}
+            className={`${variant} === "single_underline" ? "single_underline" : "}`}
           />
         ) : type === "checkbox" ? (
           <div className="form-field-checkbox">
-            <input type={type} id={id} onChange={onChange} />
+            <input
+              type={type}
+              id={id}
+              defaultChecked={defaultChecked}
+              onChange={onChange}
+              onBlur={onBlur}
+            />
             <label htmlFor={id}>{placeholder}</label>
           </div>
         ) : (
@@ -23,7 +44,10 @@ export default function Input({ type, id, label, placeholder, onChange }) {
             type={type}
             id={id}
             onChange={onChange}
+            onBlur={onBlur}
             placeholder={placeholder}
+            defaultValue={defaultValue}
+            className={`${variant} === "single_underline" ? "single_underline" : "}`}
           />
         )}
       </div>
