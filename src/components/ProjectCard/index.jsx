5c153d76ecfe0d4a4b99e9dd75589c88fed74833
Miao Zhao <miao_zhao@yahoo.com>
2022-08-26 22:04:38 +1000

    Refactor `ProjectCard` with `getProjectById` service

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getUserById } from "../../services/getUserById";
 import "./ProjectCard.css";
-
+import { getProjectById } from "../../services/getProjectById";
 export default function ProjectCard({ project }) {
   const { id, image, location, owner, title } = project;
   const [ownerName, setOwnerName] = useState("");
@@ -15,24 +15,19 @@ export default function ProjectCard({ project }) {
   }, [owner]);
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        const { goal, pledges } = data;
-        let pledgeNumberArr = [];
-        if (pledges.length > 0) {
-          pledges.map(pledge => pledgeNumberArr.push(pledge.amount));
-        } else {
-          pledgeNumberArr.push(0);
-        }
-        const pledgeSum = pledgeNumberArr.reduce((accumulator, value) => {
-          return accumulator + value;
-        });
-        setPledgeStatus(((pledgeSum / goal) * 100).toFixed(0));
-      })
-      .catch(err => console.log("project err", err));
+    getProjectById(id).then(data => {
+      const { goal, pledges } = data;
+      let pledgeNumberArr = [];
+      if (pledges.length > 0) {
+        pledges.map(pledge => pledgeNumberArr.push(pledge.amount));
+      } else {
+        pledgeNumberArr.push(0);
+      }
+      const pledgeSum = pledgeNumberArr.reduce((accumulator, value) => {
+        return accumulator + value;
+      });
+      setPledgeStatus(((pledgeSum / goal) * 100).toFixed(0));
+    });
   }, [id]);
 
   return (
@@ -45,7 +40,7 @@ export default function ProjectCard({ project }) {
             <p className="pledge-bar-percentage-number">{pledgeStatus}%</p>
             <div
               className="pledge-bar-percentage-bar"
-              style={{ width: `${pledgeStatus}%` }} // better way?
+              style={{ width: `${pledgeStatus}%` }}
             ></div>
           </div>
         </div>
