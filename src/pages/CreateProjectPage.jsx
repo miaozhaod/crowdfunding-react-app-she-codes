19d7f5566fc973cd855ec161fea97c7b8388616a
Miao Zhao <miao_zhao@yahoo.com>
2022-08-26 22:12:02 +1000

    Add `login` check in `CreateProjectPage`

--- src/pages/CreateProjectPage.jsx
+++ src/pages/CreateProjectPage.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Container from "../components/common/Container";
 import Banner from "../components/common/Banner";
 import CreateProjectForm from "../components/CreateProjectForm";
 
 export default function CreateProjectPage() {
-  return (
-    <>
-      <Container bg={true} variant="banner">
-        <Banner heading="Plan your exhibition">
-          Let’s start to plan for your exihibition now!
-        </Banner>
-      </Container>
-      <Container>
-        <CreateProjectForm />
-      </Container>
-    </>
-  );
+  const loginStatus = window.localStorage.getItem("login");
+  if (loginStatus) {
+    return (
+      <>
+        <Container bg={true} variant="banner">
+          <Banner heading="Plan your exhibition">
+            Let’s start to plan for your exihibition now!
+          </Banner>
+        </Container>
+        <Container>
+          <CreateProjectForm />
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Container bg={true} variant="banner">
+          <Banner heading="Login first to create a project">
+            <Link to="/login">Login now</Link>
+          </Banner>
+        </Container>
+      </>
+    );
+  }
 }
